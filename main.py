b5e98b07b5b8aa9bf1d3ed6166536da3891873e2
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-06-03 20:54:27 -0400

    completed the working of home page and post page

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
+api_endpoint = "https://api.npoint.io/8152213a3b7dd208d23f"
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    data = requests.get(url=api_endpoint).json()
+    return render_template("index.html",posts = data)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/post')
-def post():
-    return render_template("post.html")
+@app.route('/post/<id_no>')
+def post(id_no):
+    data = requests.get(url=api_endpoint).json()
+    return render_template("post.html", id_no = int(id_no)-1, posts = data)
 
 @app.route('/contact')
 def contact():
